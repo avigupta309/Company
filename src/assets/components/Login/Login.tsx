6e80f6008cad409f1b80bda5c5c9791d5c0b411c
Abhinash Gupta <avinashguptaa0.3@gmail.com>
2025-03-09 09:13:24 +0545

    welcome

---
+++ src/assets/components/Login/Login.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Login: React.FC = () => {
+  return (
+    <>
+      <div className="container text-8xl w-screen h-screen bg-red-400 flex justify-center items-center">
+        <div className="h-24 w-24 bg-green-400"></div>
+      </div>
+    </>
+  );
+};
